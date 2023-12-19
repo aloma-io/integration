2e59a643eb5ab5a1c40cece6e83b5e3da8f9c1b6
p1tz <p1tz@github>
2023-12-19 18:35:52 +0100

    fix

--- nodejs/src/internal/index.mjs
+++ nodejs/src/internal/index.mjs
@@ -658,7 +658,8 @@ ${text}
             return new Promise((resolve, reject) => {
               const packet = transport.newPacket(
                 {},
-                (ret) => (ret?.error ? reject(ret.error) : resolve(ret?.content)),
+                (ret) =>
+                  ret?.error ? reject(ret.error) : resolve(ret?.content),
                 `_req-${cuid()}`,
               );
 
